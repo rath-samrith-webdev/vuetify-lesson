91420de3443d47167906899a309b80470982c785
Your Name <you@example.com>
2024-05-30 11:23:28 +0700

    Intialize Lesson 1

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <NavBar />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import NavBar from "@/components/NavBar.vue";
 export default {
   name: "App",
-
   components: {
-    HelloWorld,
+    NavBar,
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app-bar color="primary" prominent>
+    <v-app-bar-nav-icon
+      variant="text"
+      @click.stop="drawer = !drawer"
+    ></v-app-bar-nav-icon>
+
+    <v-toolbar-title>My files</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <template v-if="$vuetify.display.mdAndUp">
+      <v-btn icon="mdi-magnify" variant="text"></v-btn>
+
+      <v-btn icon="mdi-filter" variant="text"></v-btn>
+    </template>
+
+    <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+  </v-app-bar>
+
+  <v-navigation-drawer
+    v-model="drawer"
+    :location="$vuetify.display.mobile ? 'bottom' : undefined"
+    temporary
+  >
+    <v-list :items="items"></v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  name: "NavBar",
+  data: () => ({
+    drawer: false,
+    group: null,
+    items: [
+      {
+        title: "Foo",
+        value: "foo",
+      },
+      {
+        title: "Bar",
+        value: "bar",
+      },
+      {
+        title: "Fizz",
+        value: "fizz",
+      },
+      {
+        title: "Buzz",
+        value: "buzz",
+      },
+    ],
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
+
+<style></style>
