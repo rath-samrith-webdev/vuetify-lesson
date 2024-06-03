e4910ca3368b1c0c3ce0c01bb58bd4a5adce66ce
Your Name <you@example.com>
2024-06-03 09:02:52 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
     <v-toolbar
-        image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-        dark
-        prominent
+      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      dark
+      prominent
     >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
@@ -15,10 +15,27 @@
         <v-icon>mdi-export</v-icon>
       </v-btn>
     </v-toolbar>
-    <v-content>
-      <h1>Router</h1>
+    <v-content class="pt-5">
+      <v-row dense>
+        <v-col v-for="(variant, i) in variants" :key="i" cols="12" md="4">
+          <v-card
+            :variant="variant"
+            class="mx-auto"
+            color="surface-variant"
+            max-width="344"
+            subtitle="Greyhound divisely hello coldly fonwderfully"
+            title="Headline"
+          >
+            <template v-slot:actions>
+              <v-btn text="Button"></v-btn>
+            </template>
+          </v-card>
+
+          <div class="text-center text-caption">{{ variant }}</div>
+        </v-col>
+      </v-row>
     </v-content>
-    <v-footer border></v-footer>
+    <v-footer border>Copy right Rath</v-footer>
   </v-app>
 </template>
 
@@ -27,7 +44,7 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    variants: ["elevated", "flat", "tonal", "outlined", "text", "plain"],
   }),
 };
 </script>
